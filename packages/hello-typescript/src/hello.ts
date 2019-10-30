190d8cd29dc01e914a74aada0391de4ec2184607
aggresss <aggresss@163.com>
2019-10-30 21:23:05 +0800

    2019-10-30 21:23:05 CST W43D3

--- packages/hello-typescript/src/hello.ts
+++ packages/hello-typescript/src/hello.ts
@@ -1,16 +1,16 @@
 class Car {
-   // 字段
-   engine:string;
+// 字段
+engine:string;
 
-   // 构造函数
-   constructor(engine:string) {
-      this.engine = engine
-   }
+// 构造函数
+constructor(engine:string) {
+  this.engine = engine
+}
 
-   // 方法
-   disp():void {
-      console.log("函数中显示发动机型号  :   "+this.engine)
-   }
+// 方法
+disp():void {
+  console.log("函数中显示发动机型号  :   "+this.engine)
+}
 }
 
 // 创建一个对象
