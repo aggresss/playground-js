e81f5af8896e57b318385b6fd833be7888f15111
aggresss <aggresss@163.com>
2019-10-31 00:16:49 +0800

    2019-10-31 00:16:49 CST W43D4

--- packages/hello-promise/src/index.js
+++ packages/hello-promise/src/index.js
@@ -1,14 +1,22 @@
-let sleep = function (delay) {
-  return new Promise((resolve, reject) => {
-   setTimeout(() => {
-     try {
-      resolve(1)
-     } catch (e) {
-      reject(0)
-     }
-   }, delay);
-  })
-}
+'use strict';
 
-sleep(5000);
+new Promise(function (resolve, reject) {
+    console.log('start new Promise...');
+    var timeOut = Math.random() * 2;
+    console.log('set timeout to: ' + timeOut + ' seconds.');
+    setTimeout(function () {
+        if (timeOut < 1) {
+            console.log('call resolve()...');
+            resolve('200 OK');
+        }
+        else {
+            console.log('call reject()...');
+            reject('timeout in ' + timeOut + ' seconds.');
+        }
+    }, timeOut * 1000);
+}).then(function (r) {
+    console.log('Done: ' + r);
+}).catch(function (reason) {
+    console.log('Failed: ' + reason);
+});
 
---
+++ packages/hello-promise/src/sleep.js
@@ -0,0 +1,14 @@
+let sleep = function (delay) {
+  return new Promise((resolve, reject) => {
+   setTimeout(() => {
+     try {
+      resolve(1)
+     } catch (e) {
+      reject(0)
+     }
+   }, delay);
+  })
+}
+
+sleep(5000);
+
