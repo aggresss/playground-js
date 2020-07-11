7aa3c7cdb6d06ffa0c746d76fe2747c343f2f7a1
aggresss <aggresss@163.com>
2020-07-11 10:57:28 +0800

    2020-07-11 10:57:28 CST W27D6

--- test/eslint/hello.js
+++ test/eslint/hello.js
@@ -13,4 +13,25 @@ function getRandomId(length) {
   return result.join('');
 }
 
+function getRandomKey(length) {
+  let result = [];
+  for (let i = 0; i < length; i++) {
+    let ranNum = Math.ceil(Math.random() * 63);
+    if (ranNum <= 25) {
+      result.push(String.fromCharCode(97 + ranNum));
+    } else if (ranNum <= 35) {
+      result.push(String.fromCharCode(48 + ranNum - 26));
+    } else if (ranNum <= 61) {
+      result.push(String.fromCharCode(65 + ranNum - 36));
+    } else if (ranNum <= 62) {
+      result.push(String.fromCharCode(45));
+    } else if (ranNum <= 63) {
+      result.push(String.fromCharCode(95));
+    }
+  }
+  return result.join('');
+}
+
 console.log(getRandomId(12));
+console.log(getRandomKey(40));
+
