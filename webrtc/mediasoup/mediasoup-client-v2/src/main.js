fa6d4888c78e303c3e21ba54d90cac97a78ffd87
aggresss <aggresss@163.com>
2020-07-28 15:02:52 +0800

    2020-07-28 15:02:52 CST W30D2

---
+++ webrtc/mediasoup/mediasoup-client-v2/src/main.js
@@ -0,0 +1,6 @@
+/* eslint-disable require-atomic-updates */
+import * as mediasoupClient from "mediasoup-client";
+
+console.log(mediasoupClient.isDeviceSupported());
+console.log(mediasoupClient.getDeviceInfo());
+
